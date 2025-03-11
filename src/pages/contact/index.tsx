b2e68e3bf2fc25df458972563eaac4be862890b0
“jielongjp” <82949450+jielongjp@users.noreply.github.com>
2025-03-11 21:18:45 +0000

    fix cors issue

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -4,32 +4,16 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
 export default function ContactForm() {
-  const [formStatus, setFormStatus] = useState<string | null>(null);
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const form = e.target as HTMLFormElement;
-
-    const data = new FormData(form);
-    const response = await fetch("https://formspree.io/f/xqapkdnv", {
-      method: "POST",
-      body: data,
-    });
-
-    if (response.ok) {
-      setFormStatus("Thank you for your message! We'll get back to you soon.");
-      form.reset();
-    } else {
-      setFormStatus("Something went wrong. Please try again.");
-    }
-  };
-
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <h2 className={styles.title}>Contact the Band</h2>
-        <form onSubmit={handleSubmit} method="POST" className={styles.form}>
+        <h2 className={styles.title}>Contact Us</h2>
+        <form
+          action="https://formspree.io/f/xqapkdnv"
+          method="POST"
+          className={styles.form}
+        >
           <div className={styles.formGroup}>
             <label htmlFor="name" className={styles.label}>
               Name:
@@ -74,8 +58,6 @@ export default function ContactForm() {
           <button type="submit" className={styles.submitButton}>
             Send Message
           </button>
-
-          {formStatus && <p className={styles.formStatus}>{formStatus}</p>}
         </form>
       </main>
       <Footer />
