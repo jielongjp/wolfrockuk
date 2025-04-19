c40d3acf5d2cfbc6fa00944ac5a05c6bed30f5e3
“jielongjp” <82949450+jielongjp@users.noreply.github.com>
2025-04-19 11:35:50 +0100

    remove link

--- src/utils/gigMeta.ts
+++ src/utils/gigMeta.ts
@@ -12,10 +12,7 @@ const gigMeta: GigMeta = {
   },
   burton2024: {
     title: "Summer of WOLF at Tower Brewery, Burton-on-trent (12/07/2024)",
-    youtube: [
-      "https://www.youtube.com/embed/wSxpljGRy9E?si=rBYx1CkRWLbrkDPl",
-      "https://www.youtube.com/embed/7Tx6r6gOLf0?si=gjoKCnW6kxvqcghF",
-    ],
+    youtube: ["https://www.youtube.com/embed/wSxpljGRy9E?si=rBYx1CkRWLbrkDPl"],
   },
   wolf240: {
     title: "WOLF 240 at Tower Brewery, Burton-on-trent (09/07/2023)",
