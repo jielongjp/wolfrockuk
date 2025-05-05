7640e72500d521e6b0e4334500e43d13f0116e43
“jielongjp” <82949450+jielongjp@users.noreply.github.com>
2025-05-05 10:49:21 +0100

    add videos

--- src/utils/gigMeta.ts
+++ src/utils/gigMeta.ts
@@ -8,11 +8,16 @@ type GigMeta = {
 const gigMeta: GigMeta = {
   Rainford: {
     title: "WOLF at Rainford Junction 14/04/25",
-    youtube: ["https://www.youtube.com/embed/7Tx6r6gOLf0?si=gjoKCnW6kxvqcghF"],
+    // youtube: ["https://www.youtube.com/embed/7Tx6r6gOLf0?si=gjoKCnW6kxvqcghF"],
   },
   burton2024: {
     title: "Summer of WOLF at Tower Brewery, Burton-on-trent (12/07/2024)",
-    youtube: ["https://www.youtube.com/embed/wSxpljGRy9E?si=rBYx1CkRWLbrkDPl"],
+    youtube: [
+      "https://www.youtube.com/embed/AAc6leO7CSI?si=4b2RIN20-VZrZzS7",
+      "https://www.youtube.com/embed/jYehRJt5PVA?si=GkWEIjU2U6t7VZxz",
+      "https://www.youtube.com/embed/wSxpljGRy9E?si=rBYx1CkRWLbrkDPl",
+      "https://www.youtube.com/embed/fuWISt-qbg4?si=D4clQKeF5ZtvKrkk",
+    ],
   },
   wolf240: {
     title: "WOLF 240 at Tower Brewery, Burton-on-trent (09/07/2023)",
